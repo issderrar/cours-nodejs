acb179b15fdaf02126726d5b57dd2c200f72d7dd
Issam <issam_derrar@yahoo.fr>
2020-05-27 13:39:45 +0200

    Add tennis players api

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
 const port = 8080
-let data = require('./data.json')
+let data = require('./datas/data.json')
+let players = require('./datas/tennisplayers.json')
 
 app.get('/', function (req, res) {
     res.send('Hello World!')
@@ -20,11 +21,37 @@ app.get('/students', function (req, res) {
 })
 app.get('/students/:lastName', function (req, res) {
     const {lastName} = req.params
-    let result = data.students.filter(
+    let result = data.students.find(
         (student) => student.lastName === lastName)
-    if (result.length > 0) {
+
+    if (result) {
         res.send(result)
-        console.log(result)
+    } else {
+        res.sendStatus(404)
+    }
+})
+
+app.get('/players', function (req, res) {
+    res.send(players)
+})
+app.get('/players/find/:lastName', function (req, res) {
+    const {lastName} = req.params
+    let result = players.players.find(
+        (player) => player.lastname === lastName
+    )
+    if (result) {
+        res.send(result)
+    } else {
+        res.sendStatus(404)
+    }
+})
+app.get('/players/:id', function (req, res) {
+    const {id} = req.params
+    let result = players.players.find(
+        (player) => player.id === Number(id)
+    )
+    if (result) {
+        res.send(result)
     } else {
         res.sendStatus(404)
     }
